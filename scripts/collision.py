6ba52e11350b6abdc1ca6ffce69c59f2a9316557
REVOLVOTRON <nickolay2021@gmail.com>
2025-01-22 18:07:57 +0900

    Почти пофиксил коллизии

--- scripts/collision.py
+++ scripts/collision.py
@@ -2,10 +2,20 @@ import pygame
 
 class CollisionLevel1:
     def __init__(self):
-        self.ladders = [pygame.Rect(514, 1059, 50, 230), pygame.Rect(2423, 431, 50, 200)]
+        self.ladders = [pygame.Rect(514, 1059, 50, 230), pygame.Rect(2423, 431, 50, 250),pygame.Rect(961, 323, 50, 25)]
         self.ground_y = 1235
-        self.walls = [
-            pygame.Rect(600, 1120, 500, 250),
+        self.walls = [pygame.Rect(591, 1080, 500, 50),
+            pygame.Rect(258, 342, 700, 50),
+            pygame.Rect(0, 717, 650, 50),
+            pygame.Rect(1671, 459, 750, 50),
+            pygame.Rect(780, 633, 800, 50),
+            pygame.Rect(2180, 752, 700, 50),
+            pygame.Rect(1022, 180, 700, 50),
+            pygame.Rect(1827, 188, 700, 50),
+            pygame.Rect(1315, 950, 750, 50),
+            pygame.Rect(0, 0, 2, 1500),
+            pygame.Rect(2559 , 0, 2, 1500)
+
         ]
         self.platforms = [
             pygame.Rect(591, 1060, 500, 50),
@@ -13,10 +23,10 @@ class CollisionLevel1:
             pygame.Rect(0, 697, 650, 50),
             pygame.Rect(1671, 439, 750, 50),
             pygame.Rect(780, 613, 800, 50),
-            pygame.Rect(2180, 732, 700, 50),
+            pygame.Rect(2180, 732, 730, 50),
             pygame.Rect(1022, 168, 700, 50),
-            pygame.Rect(1827, 168, 700, 50),
-            pygame.Rect(1315, 934, 750, 50)
+            pygame.Rect(1827, 168, 730, 50),
+            pygame.Rect(1315, 934, 780, 50)
         ]
 
         self.boxes = [
@@ -25,10 +35,10 @@ class CollisionLevel1:
             pygame.Rect(1177, 1232, 90, 90),
             pygame.Rect(1969, 847, 90, 90),
             pygame.Rect(2128, 1144, 90, 90),
-            pygame.Rect(1500, 900, 90, 90),
+
             pygame.Rect(2084, 1227, 90, 90),
             pygame.Rect(2170, 1227, 90, 90),
-            pygame.Rect(1500, 900, 90, 90),
+            pygame.Rect(1620, 1220, 80, 90),
             pygame.Rect(1162, 520, 70, 90),
             pygame.Rect(460, 235, 70, 90),
         ]
@@ -38,19 +48,26 @@ class CollisionLevel1:
         for wall in self.walls:
             if player.rect.colliderect(wall):
                 player.collision_type = 'wall'
-
-
                 if player.velocity_x > 0:
                     if player.rect.right >= wall.left:
                         player.rect.right = wall.left
-                        player.velocity_x = 0
+                        player.velocity_y = -10
                         return True
 
 
-                elif player.velocity_x < 0:
+                if player.velocity_y > 0:
+                    if player.rect.right >= wall.left:
+                        player.rect.right = wall.left
+                        player.velocity_x = 0
+                        player.velocity_y = 7
+                        return True
+
+
+                elif player.velocity_y < 0:
                     if player.rect.left <= wall.right:
                         player.rect.left = wall.right
                         player.velocity_x = 0
+                        player.velocity_y = 7
                         return True
 
         return False
@@ -79,11 +96,12 @@ class CollisionLevel1:
     def check_platform_collision(self, player):
         for platform in self.platforms:
             if platform.colliderect(player.rect):
+                player.on_ground = True
                 player.collision_type = 'platform'
-                if player.velocity_y >= 0:  
+                if player.velocity_y > 0:
                     player.rect.bottom = platform.top
-                    player.velocity_y = 0
-                    player.on_ground = True  
+                    player.velocity_y = -1.5
+                    player.on_ground = True
                     return True
         return False
 
@@ -91,19 +109,21 @@ class CollisionLevel1:
         for box in self.boxes:
             if player.rect.colliderect(box):
                 player.collision_type = 'box'
-                if player.velocity_y >= 0:  
+                if player.velocity_y >= 0:
                     player.rect.bottom = box.top
-                    player.velocity_y = 0
-                    player.on_ground = True  
+                    player.velocity_y = -1
+                    player.velocity_x = 0
+                    player.on_ground = True
                     return True
         return False
+
 
     def draw_collision_debug(self, screen):
 
         for ladder in self.ladders:
             pygame.draw.rect(screen, (255, 0, 0), ladder, 6)
         for wall in self.walls:
-            pygame.draw.rect(screen, (0, 0, 0), wall, 6)
+            pygame.draw.rect(screen, (255, 255, 0), wall, 6)
         for platform in self.platforms:
             pygame.draw.rect(screen, (0, 255, 0), platform, 6)
         for box in self.boxes:
